7b45ca71aebc618a0db234a903450f9d040f64b3
Kolangi <kolangi.kelly@yahoo.com>
2018-05-31 14:45:35 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+
+public class Main {
+    private static String name;
+
+
+    public static void main(String[] args) {
+        String firstInitial = "";
+        String lastName = "";
+        String houseNumber = "";
+        String streetName = "";
+        String State = "";
+        String city = "";
+        String name = "";
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("firstInitial: ");
+        firstInitial = keyboard.nextLine();
+        System.out.println("lastName:");
+        lastName = keyboard.nextLine();
+        System.out.println("houseNumber:");
+        houseNumber = keyboard.nextLine();
+        System.out.println("streetName:");
+        streetName = keyboard.nextLine();
+        System.out.println("State:");
+        State = keyboard.nextLine();
+        System.out.println("city:");
+        city = keyboard.nextLine();
+
+
+
+
+
+
+
+
+
+
+    }
+}
